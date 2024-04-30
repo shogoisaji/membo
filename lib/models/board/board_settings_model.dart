b2aced66aae2440f3d5fa0c1a73f952bccc44312
shogoisaji <simacsimac9@gmail.com>
2024-04-30 15:30:17 +0900

    ✨ Add board model

---
+++ lib/models/board/board_settings_model.dart
@@ -0,0 +1,20 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'board_settings_model.freezed.dart';
+part 'board_settings_model.g.dart';
+
+const double defaultHeight = 2000;
+const double defaultWidth = 2000;
+
+@freezed
+class BoardSettingsModel with _$BoardSettingsModel {
+  const factory BoardSettingsModel({
+    @Default(defaultHeight) double height,
+    @Default(defaultWidth) double width,
+    @Default('#ffffff') String bgColor,
+    @Default(false) bool isPublished,
+  }) = _BoardSettingsModel;
+
+  factory BoardSettingsModel.fromJson(Map<String, dynamic> json) =>
+      _$BoardSettingsModelFromJson(json);
+}
