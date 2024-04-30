1e05a081657547417eb08bc4c8cdc3394ebc68a3
shogoisaji <simacsimac9@gmail.com>
2024-04-30 15:06:34 +0900

    ✨ Add object model

---
+++ lib/models/board/object/object_model.dart
@@ -0,0 +1,31 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'object_model.freezed.dart';
+part 'object_model.g.dart';
+
+enum ObjectType {
+  image,
+  text,
+  stamp,
+}
+
+@freezed
+class ObjectModel with _$ObjectModel {
+  const factory ObjectModel({
+    required String objectId,
+    required ObjectType type,
+    required double positionX,
+    required double positionY,
+    required double angle,
+    required double scale,
+    required String bgColor,
+    String? imageUrl,
+    String? text,
+    int? stampId,
+    required String createrId,
+    required DateTime createdAt,
+  }) = _ObjectModel;
+
+  factory ObjectModel.fromJson(Map<String, dynamic> json) =>
+      _$ObjectModelFromJson(json);
+}
---
+++ lib/models/board/object/object_model.freezed.dart
@@ -0,0 +1,394 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'object_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+ObjectModel _$ObjectModelFromJson(Map<String, dynamic> json) {
+  return _ObjectModel.fromJson(json);
+}
+
+/// @nodoc
+mixin _$ObjectModel {
+  String get objectId => throw _privateConstructorUsedError;
+  ObjectType get type => throw _privateConstructorUsedError;
+  double get positionX => throw _privateConstructorUsedError;
+  double get positionY => throw _privateConstructorUsedError;
+  double get angle => throw _privateConstructorUsedError;
+  double get scale => throw _privateConstructorUsedError;
+  String get bgColor => throw _privateConstructorUsedError;
+  String? get imageUrl => throw _privateConstructorUsedError;
+  String? get text => throw _privateConstructorUsedError;
+  int? get stampId => throw _privateConstructorUsedError;
+  String get createrId => throw _privateConstructorUsedError;
+  DateTime get createdAt => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $ObjectModelCopyWith<ObjectModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ObjectModelCopyWith<$Res> {
+  factory $ObjectModelCopyWith(
+          ObjectModel value, $Res Function(ObjectModel) then) =
+      _$ObjectModelCopyWithImpl<$Res, ObjectModel>;
+  @useResult
+  $Res call(
+      {String objectId,
+      ObjectType type,
+      double positionX,
+      double positionY,
+      double angle,
+      double scale,
+      String bgColor,
+      String? imageUrl,
+      String? text,
+      int? stampId,
+      String createrId,
+      DateTime createdAt});
+}
+
+/// @nodoc
+class _$ObjectModelCopyWithImpl<$Res, $Val extends ObjectModel>
+    implements $ObjectModelCopyWith<$Res> {
+  _$ObjectModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? objectId = null,
+    Object? type = null,
+    Object? positionX = null,
+    Object? positionY = null,
+    Object? angle = null,
+    Object? scale = null,
+    Object? bgColor = null,
+    Object? imageUrl = freezed,
+    Object? text = freezed,
+    Object? stampId = freezed,
+    Object? createrId = null,
+    Object? createdAt = null,
+  }) {
+    return _then(_value.copyWith(
+      objectId: null == objectId
+          ? _value.objectId
+          : objectId // ignore: cast_nullable_to_non_nullable
+              as String,
+      type: null == type
+          ? _value.type
+          : type // ignore: cast_nullable_to_non_nullable
+              as ObjectType,
+      positionX: null == positionX
+          ? _value.positionX
+          : positionX // ignore: cast_nullable_to_non_nullable
+              as double,
+      positionY: null == positionY
+          ? _value.positionY
+          : positionY // ignore: cast_nullable_to_non_nullable
+              as double,
+      angle: null == angle
+          ? _value.angle
+          : angle // ignore: cast_nullable_to_non_nullable
+              as double,
+      scale: null == scale
+          ? _value.scale
+          : scale // ignore: cast_nullable_to_non_nullable
+              as double,
+      bgColor: null == bgColor
+          ? _value.bgColor
+          : bgColor // ignore: cast_nullable_to_non_nullable
+              as String,
+      imageUrl: freezed == imageUrl
+          ? _value.imageUrl
+          : imageUrl // ignore: cast_nullable_to_non_nullable
+              as String?,
+      text: freezed == text
+          ? _value.text
+          : text // ignore: cast_nullable_to_non_nullable
+              as String?,
+      stampId: freezed == stampId
+          ? _value.stampId
+          : stampId // ignore: cast_nullable_to_non_nullable
+              as int?,
+      createrId: null == createrId
+          ? _value.createrId
+          : createrId // ignore: cast_nullable_to_non_nullable
+              as String,
+      createdAt: null == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$ObjectModelImplCopyWith<$Res>
+    implements $ObjectModelCopyWith<$Res> {
+  factory _$$ObjectModelImplCopyWith(
+          _$ObjectModelImpl value, $Res Function(_$ObjectModelImpl) then) =
+      __$$ObjectModelImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String objectId,
+      ObjectType type,
+      double positionX,
+      double positionY,
+      double angle,
+      double scale,
+      String bgColor,
+      String? imageUrl,
+      String? text,
+      int? stampId,
+      String createrId,
+      DateTime createdAt});
+}
+
+/// @nodoc
+class __$$ObjectModelImplCopyWithImpl<$Res>
+    extends _$ObjectModelCopyWithImpl<$Res, _$ObjectModelImpl>
+    implements _$$ObjectModelImplCopyWith<$Res> {
+  __$$ObjectModelImplCopyWithImpl(
+      _$ObjectModelImpl _value, $Res Function(_$ObjectModelImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? objectId = null,
+    Object? type = null,
+    Object? positionX = null,
+    Object? positionY = null,
+    Object? angle = null,
+    Object? scale = null,
+    Object? bgColor = null,
+    Object? imageUrl = freezed,
+    Object? text = freezed,
+    Object? stampId = freezed,
+    Object? createrId = null,
+    Object? createdAt = null,
+  }) {
+    return _then(_$ObjectModelImpl(
+      objectId: null == objectId
+          ? _value.objectId
+          : objectId // ignore: cast_nullable_to_non_nullable
+              as String,
+      type: null == type
+          ? _value.type
+          : type // ignore: cast_nullable_to_non_nullable
+              as ObjectType,
+      positionX: null == positionX
+          ? _value.positionX
+          : positionX // ignore: cast_nullable_to_non_nullable
+              as double,
+      positionY: null == positionY
+          ? _value.positionY
+          : positionY // ignore: cast_nullable_to_non_nullable
+              as double,
+      angle: null == angle
+          ? _value.angle
+          : angle // ignore: cast_nullable_to_non_nullable
+              as double,
+      scale: null == scale
+          ? _value.scale
+          : scale // ignore: cast_nullable_to_non_nullable
+              as double,
+      bgColor: null == bgColor
+          ? _value.bgColor
+          : bgColor // ignore: cast_nullable_to_non_nullable
+              as String,
+      imageUrl: freezed == imageUrl
+          ? _value.imageUrl
+          : imageUrl // ignore: cast_nullable_to_non_nullable
+              as String?,
+      text: freezed == text
+          ? _value.text
+          : text // ignore: cast_nullable_to_non_nullable
+              as String?,
+      stampId: freezed == stampId
+          ? _value.stampId
+          : stampId // ignore: cast_nullable_to_non_nullable
+              as int?,
+      createrId: null == createrId
+          ? _value.createrId
+          : createrId // ignore: cast_nullable_to_non_nullable
+              as String,
+      createdAt: null == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$ObjectModelImpl implements _ObjectModel {
+  const _$ObjectModelImpl(
+      {required this.objectId,
+      required this.type,
+      required this.positionX,
+      required this.positionY,
+      required this.angle,
+      required this.scale,
+      required this.bgColor,
+      this.imageUrl,
+      this.text,
+      this.stampId,
+      required this.createrId,
+      required this.createdAt});
+
+  factory _$ObjectModelImpl.fromJson(Map<String, dynamic> json) =>
+      _$$ObjectModelImplFromJson(json);
+
+  @override
+  final String objectId;
+  @override
+  final ObjectType type;
+  @override
+  final double positionX;
+  @override
+  final double positionY;
+  @override
+  final double angle;
+  @override
+  final double scale;
+  @override
+  final String bgColor;
+  @override
+  final String? imageUrl;
+  @override
+  final String? text;
+  @override
+  final int? stampId;
+  @override
+  final String createrId;
+  @override
+  final DateTime createdAt;
+
+  @override
+  String toString() {
+    return 'ObjectModel(objectId: $objectId, type: $type, positionX: $positionX, positionY: $positionY, angle: $angle, scale: $scale, bgColor: $bgColor, imageUrl: $imageUrl, text: $text, stampId: $stampId, createrId: $createrId, createdAt: $createdAt)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$ObjectModelImpl &&
+            (identical(other.objectId, objectId) ||
+                other.objectId == objectId) &&
+            (identical(other.type, type) || other.type == type) &&
+            (identical(other.positionX, positionX) ||
+                other.positionX == positionX) &&
+            (identical(other.positionY, positionY) ||
+                other.positionY == positionY) &&
+            (identical(other.angle, angle) || other.angle == angle) &&
+            (identical(other.scale, scale) || other.scale == scale) &&
+            (identical(other.bgColor, bgColor) || other.bgColor == bgColor) &&
+            (identical(other.imageUrl, imageUrl) ||
+                other.imageUrl == imageUrl) &&
+            (identical(other.text, text) || other.text == text) &&
+            (identical(other.stampId, stampId) || other.stampId == stampId) &&
+            (identical(other.createrId, createrId) ||
+                other.createrId == createrId) &&
+            (identical(other.createdAt, createdAt) ||
+                other.createdAt == createdAt));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      objectId,
+      type,
+      positionX,
+      positionY,
+      angle,
+      scale,
+      bgColor,
+      imageUrl,
+      text,
+      stampId,
+      createrId,
+      createdAt);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$ObjectModelImplCopyWith<_$ObjectModelImpl> get copyWith =>
+      __$$ObjectModelImplCopyWithImpl<_$ObjectModelImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$ObjectModelImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _ObjectModel implements ObjectModel {
+  const factory _ObjectModel(
+      {required final String objectId,
+      required final ObjectType type,
+      required final double positionX,
+      required final double positionY,
+      required final double angle,
+      required final double scale,
+      required final String bgColor,
+      final String? imageUrl,
+      final String? text,
+      final int? stampId,
+      required final String createrId,
+      required final DateTime createdAt}) = _$ObjectModelImpl;
+
+  factory _ObjectModel.fromJson(Map<String, dynamic> json) =
+      _$ObjectModelImpl.fromJson;
+
+  @override
+  String get objectId;
+  @override
+  ObjectType get type;
+  @override
+  double get positionX;
+  @override
+  double get positionY;
+  @override
+  double get angle;
+  @override
+  double get scale;
+  @override
+  String get bgColor;
+  @override
+  String? get imageUrl;
+  @override
+  String? get text;
+  @override
+  int? get stampId;
+  @override
+  String get createrId;
+  @override
+  DateTime get createdAt;
+  @override
+  @JsonKey(ignore: true)
+  _$$ObjectModelImplCopyWith<_$ObjectModelImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
