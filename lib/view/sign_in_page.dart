437672f5ae3671263a4530450211ab25aab2a279
shogoisaji <simacsimac9@gmail.com>
2024-05-24 18:56:09 +0900

    🔖 Increment version

--- lib/view/sign_in_page.dart
+++ lib/view/sign_in_page.dart
@@ -6,7 +6,6 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:membo/exceptions/app_exception.dart';
 import 'package:membo/gen/assets.gen.dart';
 import 'package:membo/settings/color.dart';
 import 'package:membo/settings/text_theme.dart';
